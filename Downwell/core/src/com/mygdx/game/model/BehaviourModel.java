fb8ef3bc73c5f2d74c5f494dce1b118963db92b8
MeBeeDaisy <up201505505@fe.up.pt>
2017-05-01 17:29:59 +0100

    behaviour

---
+++ Downwell/core/src/com/mygdx/game/model/BehaviourModel.java
@@ -0,0 +1,9 @@
+package com.mygdx.game.model;
+
+/**
+ * Created by mc-guida on 30-04-2017.
+ */
+
+public abstract class BehaviourModel {
+    public abstract float[] act(float x, float y, HeroModel model);
+}
