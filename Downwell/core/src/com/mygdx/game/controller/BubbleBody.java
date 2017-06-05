0218bd45927110d301d1ca6f1fc9e7bd2af99044
MeBeeDaisy <up201505505@gcloud.fe.up.pt>
2017-06-05 19:39:12 +0100

    Started documenting code..yee

--- Downwell/core/src/com/mygdx/game/controller/BubbleBody.java
+++ Downwell/core/src/com/mygdx/game/controller/BubbleBody.java
@@ -4,15 +4,21 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.model.*;
 
-
+/**
+ * BubbleBody is a class that contains the information about with the physics body of the Bubble Enemy.
+ * @see ElementBody
+ */
 public class BubbleBody extends ElementBody {
-
+    /**
+     * Constructor that creates the fixture for the Bubble. It gives it it's attributes and constructs it's fixture.
+     * @param world the physics world
+     * @param model the model of the bubble to be used in the super class constructor
+     */
     public BubbleBody(World world, BubbleModel model) {
         super(world,model, BodyDef.BodyType.DynamicBody);
 
         float density = 1f, friction = 0.4f, restitution = 0f;
         float width = 0.4f, height = 0.4f;
-        //int dimension = GameController.TILE_DIMENSIONS;
         createRectangleFixture(body,width,height,density,friction,restitution,true);
     }
 }
--- Downwell/core/src/com/mygdx/game/controller/SnailBody.java
+++ Downwell/core/src/com/mygdx/game/controller/SnailBody.java
@@ -4,8 +4,16 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.model.SnailModel;
 
+/**
+ * SnailBody is a class that contains the information about with the physics body of the Snail Enemy.
+ * @see ElementBody
+ */
 public class SnailBody extends ElementBody {
-
+    /**
+     * Constructor that creates the fixture for the Snail. It gives it it's attributes and constructs it's fixture.
+     * @param world the physics world
+     * @param model the model of the bat to be used in the super class constructor
+     */
     public SnailBody(World world, SnailModel model) {
         super(world,model, BodyDef.BodyType.DynamicBody);
 
